f92856c9ee3840c342157badf9387a891b512213
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-06-23 20:38:09 +0600

    Код задания 2 занятия 5

---
+++ Lesson-5/Problem-2/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+    int Anton, Boris, Victor;
+    scanf("%d%d%d", &Anton, &Boris, &Victor);
+    if (Anton == Boris && Boris == Victor) {
+            printf("Same age");
+    }
+    else if (Anton > Boris && Anton > Victor) {
+            printf("Anton");
+    }
+    else if (Boris > Anton && Boris > Victor) {
+            printf("Boris");
+    }
+    else if (Victor > Anton && Victor > Boris) {
+            printf("Victor");
+    }
+    else if (Anton == Boris)  {
+            printf("Victor");
+    }
+    else if (Anton == Victor) {
+            printf("Boris");
+    }
+    else if (Boris == Victor) {
+            printf("Anton");
+    }
+}
+
+
