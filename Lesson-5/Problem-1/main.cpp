cde5a516ca4fcbf66b677a653482e3920e2ea836
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-06-25 18:31:01 +0600

    Поменяла код

--- Lesson-5/Problem-1/main.cpp
+++ Lesson-5/Problem-1/main.cpp
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main () {
-    int a, b, c, m;
+    int a, b, c, max;
     scanf("%d%d%d", &a, &b, &c);
-    if(a >= b && a >= c) {
-        m = a;
+    if(a > b) {
+        max = a;
+    } else {
+        max = b;
     }
-    if(b >= a && b >= c) {
-        m = b;
+    if(c > max) {
+        max = c;
     }
-    if(c >= b && c >= a) {
-        m = c;
-    }
-    printf("%d", m);
+    printf("%d", max);
 }
