8a47c6c29eba5e787851fc4b9f14a170ae9b305b
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-02 19:24:48 +0600

    Исправила расположение "else"

--- Lesson-5/Problem-2/main.cpp
+++ Lesson-5/Problem-2/main.cpp
@@ -5,23 +5,17 @@ int main () {
     scanf("%d%d%d", &Anton, &Boris, &Victor);
     if (Anton == Boris && Boris == Victor) {
             printf("Same age");
-    }
-    else if (Anton > Boris && Anton > Victor) {
+    } else if (Anton > Boris && Anton > Victor) {
             printf("Anton");
-    }
-    else if (Boris > Anton && Boris > Victor) {
+    } else if (Boris > Anton && Boris > Victor) {
             printf("Boris");
-    }
-    else if (Victor > Anton && Victor > Boris) {
+    } else if (Victor > Anton && Victor > Boris) {
             printf("Victor");
-    }
-    else if (Anton == Boris)  {
+    } else if (Anton == Boris) {
             printf("Victor");
-    }
-    else if (Anton == Victor) {
+    } else if (Anton == Victor) {
             printf("Boris");
-    }
-    else if (Boris == Victor) {
+    } else if (Boris == Victor) {
             printf("Anton");
     }
 }
