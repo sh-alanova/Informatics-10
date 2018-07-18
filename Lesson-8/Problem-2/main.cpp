424bbd72826f0c926f56905fc280821b660e3df9
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-18 22:07:59 +0600

    Учла два отрицательных числа

--- Lesson-8/Problem-2/main.cpp
+++ Lesson-8/Problem-2/main.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main () {
-  int a, b;
-	scanf("%d%d", &a, &b); 
-  int comp = 0, count = 0;
-	if (a < 0 && a < b) {
+    int a, b, comp = 0, count = 0;
+	scanf("%d%d", &a, &b);
+	if (b < 0 && a < 0) {
+	while(count < (- a)) {
+        comp = comp + (- b);
+        ++ count;
+	}
+	printf("(%d)*(%d)=%d", a, b, comp);
+	} else if (a < 0 && a < b) {
 	while(count < b) {
         comp = comp + a;
         ++ count;
-}
+	}
 	printf("(%d)*%d=%d", a, b, comp);
 	} else if (a >= 0 && a < b) {
 	while(count < b) {
