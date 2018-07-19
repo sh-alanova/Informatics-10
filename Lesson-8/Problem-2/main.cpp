60ab122af2d530784d5e19b81b879bc9260937b5
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-19 21:33:56 +0600

    Update main.cpp

--- Lesson-8/Problem-2/main.cpp
+++ Lesson-8/Problem-2/main.cpp
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
 int main () {
-	int a, b, comp = 0, count = 0;
-	scanf("%d%d", &a, &b);
-	if (b < 0 && a < 0) {
-	while(count < (- a)) {
-	comp = comp + (- b);
-	++ count;
-	}
-	printf("(%d)*(%d)=%d", a, b, comp);
-	} else if (a < 0 && a < b) {
-	while(count < b) {
-	comp = comp + a;
-	++ count;
-	}
-	printf("(%d)*%d=%d", a, b, comp);
-	} else if (a >= 0 && a < b) {
-	while(count < b) {
-	comp = comp + a;
-	++ count;
-	}
-	printf("%d*%d=%d", a, b, comp);
-	} else if (b < 0 && b < a) {
-	while(count < a) {
-	comp = comp + b;
-	++ count;
-	}
-	printf("%d*(%d)=%d", a, b, comp);
-	} else if ((b >= 0 && b < a) || (a == b)) {
-	while(count < a) {
-	comp = comp + b;
-	++ count;
-	}
-	printf("%d*%d=%d", a, b, comp);
-	}
-}
+    int a, b, comp = 0, count = 0;
+    scanf("%d%d", &a, &b);
+    if (b < 0 && a < 0) {
+    while(count < (- a)) {
+    comp = comp + (- b);
+    ++count;
+    }
+    printf("(%d)*(%d)=%d", a, b, comp);
+    } else if (a < 0 && a < b) {
+    while(count < b) {
+    comp = comp + a;
+    ++count;
+    }
+    printf("(%d)*%d=%d", a, b, comp);
+    } else if (a >= 0 && a < b) {
+    while(count < b) {
+    comp = comp + a;
+    ++count;
+    }
+    printf("%d*%d=%d", a, b, comp);
+    } else if (b < 0 && b < a) {
+    while(count < a) {
+    comp = comp + b;
+    ++count;
+    }
+    printf("%d*(%d)=%d", a, b, comp);
+    } else if ((b >= 0 && b < a) || (a == b)) {
+    while(count < a) {
+    comp = comp + b;
+    ++count;
+    }
+    printf("%d*%d=%d", a, b, comp);
+    }
+    }
 
 
