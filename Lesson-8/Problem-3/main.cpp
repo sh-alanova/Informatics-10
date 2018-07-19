50f4b636c1eb12b379a5fcf38e8ad0c37c9ce009
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-19 21:32:22 +0600

    Update main.cpp

--- Lesson-8/Problem-3/main.cpp
+++ Lesson-8/Problem-3/main.cpp
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main () {
-	int n;
-	scanf("%d", &n);
-	int f1 = 0, f2 = 1, f = 0, sum = 0;
-	if(n <= 0) {
-		printf("ERROR");
-	} else {
-		while(f < n) {
-		  f = f1 + f2;
-		  sum += f2;
-		  f1 = f2;
-		  f2 = f;
-	  }
-	printf("%d", sum);
-	}
+    int n;
+    scanf("%d", &n);
+    int f1 = 0, f2 = 1, f = 0, sum = 0;
+    if(n <= 0) {
+        printf("ERROR");
+    } else {
+        while(f < n) {
+          f = f1 + f2;
+          sum += f2;
+          f1 = f2;
+          f2 = f;
+        }
+    printf("%d", sum);
+    }
 }
 
 
