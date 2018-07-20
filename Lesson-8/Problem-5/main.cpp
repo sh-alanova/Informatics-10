6fe072f8aed9fc61a6d63e9e64f2bc437fa8af59
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-20 20:52:36 +0600

    Update main.cpp

--- Lesson-8/Problem-5/main.cpp
+++ Lesson-8/Problem-5/main.cpp
@@ -2,9 +2,9 @@
 
 int main () {
     int n;
-	scanf("%d", &n);
-	int digit_1, digit_2, count = 0;
-	while(n > 0) {
+    scanf("%d", &n);
+    int digit_1, digit_2, count = 0;
+    while(n > 0) {
         digit_1 = n % 10;
         digit_2 = n / 10 % 10;
         if(digit_1 == digit_2) {
