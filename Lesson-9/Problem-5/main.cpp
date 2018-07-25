6d68c3ea3103deeadde8ff9d46e759eb2d800010
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-07-25 18:59:45 +0600

    Код задания 5 занятия 9 про простые числа

---
+++ Lesson-9/Problem-5/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int digit, count;
+    for(int i = a; i < b; ++i) {
+            count = 0;
+        for(int j = 2; j < a; ++j) {
+            digit = i % j;
+            if (digit == 0) {
+                ++count;
+            }
+
+        }
+        if(count == 0) {
+            printf("%d\n", i);
+        }
+    }
+}
