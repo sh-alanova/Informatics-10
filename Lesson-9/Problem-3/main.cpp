381b21045100121fd7bb614712deaec4350d8a02
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-08-01 19:31:32 +0600

    Исправила отступы

--- Lesson-9/Problem-3/main.cpp
+++ Lesson-9/Problem-3/main.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int digit_1, digit_2, digit_3, cube_1, cube_2, cube_3, sum;
     for(int n = 100; n < 1000; ++n) {
-            digit_1 = n / 100;
-            digit_2 = n / 10 % 10;
-            digit_3 = n % 10;
-            cube_1 = digit_1 * digit_1 * digit_1;
-            cube_2 = digit_2 * digit_2 * digit_2;
-            cube_3 = digit_3 * digit_3 * digit_3;
-            sum = cube_1 + cube_2 + cube_3;
-            if(sum == n) {
-                    printf("%d\n", n);
+        digit_1 = n / 100;
+        digit_2 = n / 10 % 10;
+        digit_3 = n % 10;
+        cube_1 = digit_1 * digit_1 * digit_1;
+        cube_2 = digit_2 * digit_2 * digit_2;
+        cube_3 = digit_3 * digit_3 * digit_3;
+        sum = cube_1 + cube_2 + cube_3;
+        if(sum == n) {
+            printf("%d\n", n);
         }
     }
 }
