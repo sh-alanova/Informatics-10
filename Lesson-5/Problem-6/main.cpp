c923d4aa33cf56844481d60cb8b27c59d2a8f17d
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-08-02 19:15:53 +0600

    Исправила отступы

--- Lesson-5/Problem-6/main.cpp
+++ Lesson-5/Problem-6/main.cpp
@@ -4,9 +4,9 @@ int main () {
     int a, b, c;
     scanf("%d%d%d", &a, &b, &c);
     if (a + b > c && a + c > b && b + c > a) {
-       printf("YES");
-       } else {
-            printf("NO");
-       }
+        printf("YES");
+    } else {
+        printf("NO");
+    }
 }
 
