92e30937a9da14bae07dba9c70d73fb0c42674d6
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-08-02 19:26:51 +0600

    Исправила отступы

--- Lesson-10/Problem-3/main.cpp
+++ Lesson-10/Problem-3/main.cpp
@@ -3,14 +3,13 @@
 int main() {
     int n;
     scanf("%d", &n);
-    int k, digit, count_1, count_2;
     for(int i = 1; i <= n; ++i) {
         int j = i;
         while (j % 10 != 0 && i % (j % 10) == 0) {
-                j = j / 10;
-                if (j == 0) {
+            j = j / 10;
+            if (j == 0) {
                 printf("%d ", i);
-                }
+            }
         }
     }
 }
