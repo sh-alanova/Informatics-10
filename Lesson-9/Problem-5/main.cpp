4539a1b51703909065d89a5ef96d4e3b6f612245
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-08-04 19:25:01 +0600

    обновила код

--- Lesson-9/Problem-5/main.cpp
+++ Lesson-9/Problem-5/main.cpp
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <cmath>
+
+using namespace std;
 
 int main() {
     int a, b;
     scanf("%d%d", &a, &b);
-    int count;
-    for(int i = a; i <= b; ++i) {
-        count = 0;
-        for(int j = 2; j < a; ++j) {
-            if (i % j == 0) {
+    if (b < a || a < 0 || b > 100000) {
+        return 0;
+    }
+    bool first = true;
+    for(int i = a; i <= b; i++) {
+        int count = 0;
+        for(int j = 2; j * j <= i; j++) {
+            if(i % j == 0) {
                 ++count;
             }
         }
-        if(count == 0) {
-            printf("%d\n", i);
+        if (count == 0) {
+            if(first) {
+                printf("%d", i);
+                first = false;
+            } else {
+                printf(" %d", i);
+            }
         }
     }
 }
