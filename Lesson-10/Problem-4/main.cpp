3c62c0569636769e8b9f889e18dd33c8f61f49e9
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2018-08-06 20:46:49 +0600

    Код задания 4 занятия 10

--- Lesson-10/Problem-4/main.cpp
+++ Lesson-10/Problem-4/main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int n;
     scanf("%d", &n);
     int x_1, y_1, x_2, y_2;
@@ -13,12 +12,28 @@ int main()
         y1 = y2;
         x2 = x3;
         y2 = y3;
-        scanf("%d", &x3, &y3);
-        int x_vector1 = x1 - x2, y_vector1 = y1 - y2, x_vector2 = x3 - x2, y_vector2 = y3 - y2;
-        int cos = (x_vector1 * x_vector2 - y_vector1 * y_vector2);
-        if(cos == 0) {
+        scanf("%d%d", &x3, &y3);
+        int a_1 = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        int b_1 = ((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+        int c_1 = ((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+        int cos_1 = a_1 + b_1 - c_1;
+        if(cos_1 == 0) {
             ++count;
         }
+    }
+    int a_2 = ((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+    int b_2 = ((x3 - x_1) * (x3 - x_1) + (y3 - y_1) * (y3 - y_1));
+    int c_2 = ((x2 - x_1) * (x2 - x_1) + (y2 - y_1) * (y2 - y_1));
+    int cos_2 = a_2 + b_2 - c_2;
+    if(cos_2 == 0) {
+        ++count;
+    }
+    int a_3 = ((x3 - x_1) * (x3 - x_1) + (y3 - y_1) * (y3 - y_1));
+    int b_3 = ((x_1 - x_2) * (x_1 - x_2) + (y_1 - y_2) * (y_1 - y_2));
+    int c_3 = ((x3 - x_2) * (x3 - x_2) + (y3 - y_2) * (y3 - y_2));
+    int cos_3 = a_3 + b_3 - c_3;
+    if(cos_3 == 0) {
+        ++count;
     }
     printf("%d", count);
 }
